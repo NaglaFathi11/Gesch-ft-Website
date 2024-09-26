82c4be5bd9bdcba13b81c98e017f19f71784e57d
NaglaFathi11 <naglafatehi11@gmail.com>
2024-09-26 07:22:28 -0700

    Add Link

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./NotFound.css";
 
 const NotFound = () => {
@@ -7,9 +8,9 @@ const NotFound = () => {
       <p className="not-found-message">
         The page you are looking for does not exist.
       </p>
-      <a href="/" className="not-found-link">
+      <Link href="/" className="not-found-link">
         Go Back Home
-      </a>
+      </Link>
     </div>
   );
 };
