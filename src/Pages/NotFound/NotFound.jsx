ca15cda51706fa0399bb431ee402f35b9c3c4a0c
NaglaFathi11 <naglafatehi11@gmail.com>
2024-09-26 07:19:40 -0700

    Add css

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -2,9 +2,14 @@ import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="not-found-container">
+      <h1 className="not-found-title">404 - Page Not Found</h1>
+      <p className="not-found-message">
+        The page you are looking for does not exist.
+      </p>
+      <a href="/" className="not-found-link">
+        Go Back Home
+      </a>
     </div>
   );
 };
