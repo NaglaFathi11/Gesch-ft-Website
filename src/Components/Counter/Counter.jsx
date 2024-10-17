ade70461372eab07e0c5615843271705460c1845
NaglaFathi11 <naglafatehi11@gmail.com>
2024-10-17 10:07:22 -0700

    Editing performance

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,6 +1,6 @@
 import "./Counter.css";
-import PlusPhoto from "/assets/plus.svg";
-import MinusPhoto from "/assets/minus.svg";
+import PlusPhoto from "/assets/plus.webp";
+import MinusPhoto from "/assets/minus.webp";
 import { useCounterStore } from "../../Store";
 
 export default function Counter({ product }) {
@@ -8,12 +8,12 @@ export default function Counter({ product }) {
   return (
     <div className="Counter">
       <button onClick={() => decrementCount(product)}>
-        <img src={MinusPhoto} alt="Not Found" />
+        <img src={MinusPhoto} alt="Not Found" width="10px" height="1px" />
       </button>
       <label>{count}</label>
 
       <button onClick={() => incrementCount(product)}>
-        <img src={PlusPhoto} alt="Not Found" />
+        <img src={PlusPhoto} alt="Not Found" width="10px" height="1px" />
       </button>
     </div>
   );
